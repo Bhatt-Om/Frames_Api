dffc7cf3f66f681ec29b3d6c511be38f35a5ea9d
Bhatt-Om <bhattom1111@gmail.com>
2023-05-19 11:53:56 +0530

    made chages in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
 append :rbenv_map_bins, 'puma', 'pumactl'
-# append :linked_files, %w{config/credentials.yml.enc config/master.key}
+append :linked_files, %w{config/credentials.yml.enc config/master.key}
 
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 ## Defaults:
