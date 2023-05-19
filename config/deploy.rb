c226fab2c3d74035b89e52d5f2ed9bad7c969dd7
Bhatt-Om <bhattom1111@gmail.com>
2023-05-19 10:53:27 +0530

    add change in deploy.rb files

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,6 +28,7 @@ set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 
+append :linked_files, 'config/credentials.yml.enc'
 append :rbenv_map_bins, 'puma', 'pumactl'
 
 ## Defaults:
