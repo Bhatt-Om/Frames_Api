ebcf5ae5b91c4f38fcf2c9dac24473dfc862bc38
Bhatt-Om <bhattom1111@gmail.com>
2023-05-19 12:04:37 +0530

    made chages in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,13 @@ append :rbenv_map_bins, 'puma', 'pumactl'
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
+# ...
+
+set :linked_files, fetch(:linked_files, []).push('config/credentials.yml.enc')
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-append :linked_files, 'config/credentials.yml.enc'
+
+# ...
+
 ## Linked Files & Directories (Default None):
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
